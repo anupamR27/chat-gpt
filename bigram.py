f1be96d95db44885e42f668ff859ad86ed55c448
anupamR27 <172539307+anupamR27@users.noreply.github.com>
2026-08-31 10:53:09 +0530

    BiGram NN module addition

--- bigram.py
+++ bigram.py
@@ -81,4 +81,17 @@ class BigramLanguageModel(nn.Module):
         super().__init__()
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
-        
+
+    def forward(self, idx, targets=None):
+        # idx and targets are both (B,T) tensor of integers
+        logits = self.token_embedding_table(idx) # (B,T,C)
+
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
+
+        return logits, loss   
