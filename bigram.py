a5fa7f5eb5996ebc9a02eefb6d575d6e1b141fe2
anupamR27 <172539307+anupamR27@users.noreply.github.com>
2026-08-31 09:07:13 +0530

    Hyper-parameters

--- bigram.py
+++ bigram.py
@@ -2,3 +2,12 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
+# hyperparameters
+batch_size = 32
+block_size = 8
+max_iters = 3000
+eval_interval = 300
+learning_rate = 1e-2
+device = 'mps' if torch.backends.mps.is_available() else 'cpu'
+eval_iters = 200
+# ------------
