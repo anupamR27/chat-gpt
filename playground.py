c4c4c085a3c106a8bdf5aa69dcd19e250f611202
anupamR27 <172539307+anupamR27@users.noreply.github.com>
2026-08-31 18:20:50 +0530

    Add query key value attention

--- playground.py
+++ playground.py
@@ -1,6 +1,6 @@
 import torch
-from torch.nn import functional as F
 import torch.nn as nn
+from torch.nn import functional as F
 
 torch.manual_seed(1337)
 
@@ -12,6 +12,7 @@ head_size = 16
 
 key = nn.Linear(C, head_size, bias=False)
 query = nn.Linear(C, head_size, bias=False)
+value = nn.Linear(C, head_size, bias=False)
 
 print(x.shape)
 
@@ -28,12 +29,12 @@ tril = torch.tril(torch.ones(T, T))
 
 k = key(x)
 q = query(x)
+v = value(x)
 
 wei = q @ k.transpose(-2, -1)
-
 wei = wei.masked_fill(tril == 0, float('-inf'))
 wei = F.softmax(wei, dim=-1)
 
-xbow2 = wei @ x
+out = wei @ v
 
-print(xbow2)
+print(out)
