77f5f375cbd406d2d0de32ce2a12f59362af41e2
anupamR27 <172539307+anupamR27@users.noreply.github.com>
2026-08-31 16:24:57 +0530

    Playground attention using average positional arguments

--- playground.py
+++ playground.py
@@ -6,3 +6,12 @@ B, T, C = 4, 8, 2
 x = torch.randn(B, T, C)
 
 print(x.shape)
+
+xbow = torch.zeros((B, T, C))
+
+for b in range(B):
+    for t in range(T):
+        xprev = x[b, :t+1]
+        xbow[b, t] = torch.mean(xprev, 0)
+
+print(xbow)
