0192689638d8c4e5fd84124f7e74cda2f289f98f
anupamR27 <172539307+anupamR27@users.noreply.github.com>
2026-08-31 09:15:30 +0530

    char testing len

--- bigram.py
+++ bigram.py
@@ -13,3 +13,10 @@ eval_iters = 200
 # ------------
 
 torch.manual_seed(1337)
+
+
+# wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
+with open('input.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+
+print(len(text))
