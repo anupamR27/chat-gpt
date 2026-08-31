8500ba1b50c56a4a57b1a1fa374f28a15c62a4ac
anupamR27 <172539307+anupamR27@users.noreply.github.com>
2026-08-31 18:19:19 +0530

    Implement basic self-attention

--- playground.py
+++ playground.py
@@ -1,9 +1,17 @@
 import torch
+from torch.nn import functional as F
+import torch.nn as nn
 
 torch.manual_seed(1337)
 
 B, T, C = 4, 8, 2
+
 x = torch.randn(B, T, C)
+
+head_size = 16
+
+key = nn.Linear(C, head_size, bias=False)
+query = nn.Linear(C, head_size, bias=False)
 
 print(x.shape)
 
@@ -16,10 +24,16 @@ for b in range(B):
 
 print(xbow)
 
-wei = torch.tril(torch.ones(T, T))
-wei = wei / wei.sum(1, keepdim=True)
+tril = torch.tril(torch.ones(T, T))
+
+k = key(x)
+q = query(x)
+
+wei = q @ k.transpose(-2, -1)
+
+wei = wei.masked_fill(tril == 0, float('-inf'))
+wei = F.softmax(wei, dim=-1)
 
 xbow2 = wei @ x
-print(xbow2)
 
-print(torch.allclose(xbow, xbow2))
+print(xbow2)
