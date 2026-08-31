8ec78097886ac546102c12b2a73d514fd73ba879
anupamR27 <172539307+anupamR27@users.noreply.github.com>
2026-08-31 19:16:30 +0530

    Add Transformer block with layer normalization

--- bigram.py
+++ bigram.py
@@ -147,6 +147,27 @@ class FeedForward(nn.Module):
 
     def forward(self, x):
         return self.net(x)
+    
+class Block(nn.Module):
+    """Transformer block: communication followed by computation"""
+
+    def __init__(self, n_embd, n_head):
+        super().__init__()
+
+        head_size = n_embd // n_head
+
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedForward(n_embd)
+
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
+        return x
+    
+
 
     
 # super simple bigram model
@@ -158,8 +179,9 @@ class BigramLanguageModel(nn.Module):
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
 
-        self.sa_head = MultiHeadAttention(4, n_embd // 4)
-        self.ffwd = FeedForward(n_embd)
+        self.blocks = nn.Sequential(
+            Block(n_embd, n_head=4)
+        )
 
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
@@ -173,8 +195,7 @@ class BigramLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx)
         pos_emb = self.position_embedding_table(torch.arange(T, device=device))
         x = tok_emb + pos_emb
-        x = x + self.sa_head(x)
-        x = x + self.ffwd(x)
+        x = self.blocks(x)
         logits = self.lm_head(x)
 
         if targets is None:
