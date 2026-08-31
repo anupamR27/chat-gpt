111573a671696c7941292cae3ea1c519bf4fd792
anupamR27 <172539307+anupamR27@users.noreply.github.com>
2026-08-31 10:30:09 +0530

    Model Loss eval

--- bigram.py
+++ bigram.py
@@ -52,10 +52,24 @@ def get_batch(split):
     return x, y
 # Y is something always +1 from X, trying to teach the model what is happening next
 
-xb, yb = get_batch('train')
-print('inputs:')
-print(xb.shape)
-print(xb)
-print('targets:')
-print(yb.shape)
-print(yb)
+# xb, yb = get_batch('train')
+# print('inputs:')
+# print(xb.shape)
+# print(xb)
+# print('targets:')
+# print(yb.shape)
+# print(yb)
+
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
